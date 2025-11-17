ad4b723bdfbfa13269442bec16b774183848bc00
wassimdiallo67-alt <wassimdiallo67@gmail.com>
2025-11-17 18:14:20 +0000

    Remplacement

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,33 @@
-const express = require('express');
+import express from 'express';
+import { getAllComments, createComment, updateComment, deleteComment, getCommentById } from '../controllers/commentController.js';
+import { body, param, query } from 'express-validator';
+import validate from '../middlewares/validate.js';
+import { verifyToken } from '../middlewares/auth.js';
+ 
 const router = express.Router();
-const { authenticate } = require('../middlewares/auth');
-const { listComments, createComment } = require('../controllers/commentController');
+ 
+router.get('/', verifyToken, [
+  query('page').optional().isInt({ min: 1 }).withMessage('Invalid page'),
+  query('limit').optional().isInt({ min: 1 }).withMessage('Invalid limit')
+], validate, getAllComments);
+ 
+router.get('/:id', verifyToken, [
+  param('id').isInt().withMessage('Invalid ID')
+], validate, getCommentById);
+ 
+router.post('/', verifyToken, [
+  body('content').notEmpty().withMessage('Content required'),
+  body('taskId').isInt().withMessage('Invalid task ID')
+], validate, createComment);
+ 
+router.put('/:id', verifyToken, [
+  param('id').isInt().withMessage('Invalid ID'),
+  body('content').optional().notEmpty()
+], validate, updateComment);
+ 
+router.delete('/:id', verifyToken, [
+  param('id').isInt().withMessage('Invalid ID')
+], validate, deleteComment);
+ 
+export default router;
 
-router.get('/:taskId', authenticate, listComments);
-router.post('/', authenticate, createComment);
-
-module.exports = router;

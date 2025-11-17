1a76877c66b2d211e7c8c5f4330ffaae1cc85d8d
mbodj <2732129@collegelacite.ca>
2025-11-16 19:40:25 -0500

    Suppression des commentaires

--- models/Project.js
+++ models/Project.js
@@ -8,12 +8,6 @@ const projectSchema = new mongoose.Schema({
 module.exports = mongoose.model('Project', projectSchema);
 
 
-// const mongoose = require('mongoose');
 
-// const projectSchema = new mongoose.Schema({
-//     name: String,
-//     description: String,
-//     createdAt: { type: Date, default: Date.now }
-// });
 
-// module.exports = mongoose.model('Project', projectSchema);
+

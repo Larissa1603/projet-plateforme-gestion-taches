b2e2e6a75597846dd0002bca67d54d8f2511ee90
wassimdiallo67-alt <wassimdiallo67@gmail.com>
2025-11-17 05:33:20 +0000

    Remplacement de fichier

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,14 +1,42 @@
-const { Comment } = require('../models');
+const { sequelize, User } = require('../models');
+const { DataTypes } = require('sequelize');
+
+const Comment =
+  sequelize.models.Comment ||
+  require('../models/Comment')(sequelize, DataTypes);
+
+if (!Comment.associations || !Comment.associations.User) {
+  User.hasMany(Comment, { foreignKey: 'userId' });
+  Comment.belongsTo(User, { foreignKey: 'userId' });
+}
 
 const listComments = async (req, res) => {
-  const comments = await Comment.findAll({ where: { taskId: req.params.taskId } });
-  res.json(comments);
+  try {
+    const comments = await Comment.findAll({
+      where: { taskId: req.params.taskId },
+      include: [{ model: User, attributes: ['id', 'email', 'name'] }]
+    });
+
+    res.json(comments);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 const createComment = async (req, res) => {
-  const { content, taskId } = req.body;
-  const comment = await Comment.create({ content, taskId, userId: req.user.id });
-  res.status(201).json(comment);
+  try {
+    const { content, taskId } = req.body;
+
+    const comment = await Comment.create({
+      content,
+      taskId,
+      userId: req.user.id
+    });
+
+    res.status(201).json(comment);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 module.exports = { listComments, createComment };

93b2b45a37a4f64e35419a4dfdc9f6476bb19a2c
wassimdiallo67-alt <wassimdiallo67@gmail.com>
2025-11-17 19:44:10 +0000

    Remplacement

--- models/Comment.js
+++ models/Comment.js
@@ -1,8 +1,7 @@
-module.exports = (sequelize, DataTypes) => {
+export default (sequelize, DataTypes) => {
   return sequelize.define('Comment', {
-    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     content: { type: DataTypes.TEXT, allowNull: false },
     taskId: { type: DataTypes.INTEGER, allowNull: false },
     userId: { type: DataTypes.INTEGER, allowNull: false }
-  }, { tableName: 'comments', timestamps: true });
+  });
 };
